b846ddce6cf0b4f1e13fc2ce67f87f19030f4b67
ZeeraqH <luan.gaertner900@gmail.com>
2020-07-28 21:08:34 -0300

    end of second class

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Menu from './components/Menu/index'
-import BannerMain from './components/BannerMain/index'
-import Carousel from './components/Carousel/index'
-import dadosIniciais from './data/dados_iniciais.json'
-import Footer from './components/Footer/index'
+import Menu from '../../components/Menu/index'
+import BannerMain from '../../components/BannerMain/index'
+import Carousel from '../../components/Carousel/index'
+import dadosIniciais from '../../data/dados_iniciais.json'
+import Footer from '../../components/Footer/index'
 
 
-function App() {
+function Home() {
   return (
     <div style={{background: "#000000"}}>
       <Menu />
@@ -42,4 +42,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
