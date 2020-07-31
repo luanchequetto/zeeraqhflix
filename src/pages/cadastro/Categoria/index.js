6a4dcefe0f58a21d0cd7cbdc998d5151fb357495
ZeeraqH <luan.gaertner900@gmail.com>
2020-07-30 21:33:30 -0300

    server.js created to heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable linebreak-style */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../PageDefault';
 import Formfield from '../../../components/FormField';
@@ -9,10 +9,9 @@ function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
     descricao: '',
-    cor: '#FFF',
+    cor: '#000000',
   };
   const [categorias, setCategorias] = useState([]);
-
   const [values, setValues] = useState(valoresIniciais);
 
   function setValue(chave, value) {
@@ -27,6 +26,20 @@ function CadastroCategoria() {
     // const { getAttribute, value } = e.target;
     setValue(e.target.getAttribute('name'), e.target.value);
   }
+
+  useEffect(()=>{
+    console.log('alo alo brasil')
+    const URL_TOP = 'http://localhost:8080/categorias';
+    fetch(URL_TOP)
+    .then(async (respostaDoServidor) =>{
+      const resposta  = await respostaDoServidor.json();
+      setCategorias([
+        ...resposta,
+      ]);
+    });
+  }, [
+    values.descricao
+  ])
 
   return (
     <PageDefault>
@@ -73,6 +86,10 @@ function CadastroCategoria() {
           Cadastrar
         </Button>
       </form>
+      
+      <div>
+
+      </div>
 
       <ul>
         {categorias.map((categoria, indice) => (
